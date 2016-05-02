3eb0ba834bee2d413893b905d02b9a6c2933f114
samwalker <samwalker505@gmail.com>
2016-05-03 00:44:36 +0800

    backup

--- DioV/app/src/main/java/com/example/waichiuyung/diov/BaseActivity.java
+++ DioV/app/src/main/java/com/example/waichiuyung/diov/BaseActivity.java
@@ -1,11 +1,14 @@
 package com.example.waichiuyung.diov;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by waichiuyung on 18/2/16.
  */
 public class BaseActivity extends AppCompatActivity {
-
-
+    final String PREF = "default";
+    protected Object save(String key, Object val) {
+        return  val;
+    }
 }
